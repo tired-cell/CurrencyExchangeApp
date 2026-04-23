f164fa6ac38eff32969fbb700e928d5dbc9275a9
tired-cell <nbltietired@gmail.com>
2026-04-23 10:40:28 +0300

    Add Theme.kt file with currency exchange app theme

---
+++ app/src/main/java/com/sokolov/currencyexchangeapp/presentation/ui/theme/Theme.kt
@@ -0,0 +1,10 @@
+package com.sokolov.currencyexchangeapp.presentation.ui.theme
+
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.lightColorScheme
+import androidx.compose.runtime.Composable
+
+@Composable
+fun CurrencyExchangeAppTheme(content: @Composable () -> Unit) {
+    MaterialTheme(colorScheme = lightColorScheme(), content = content)
+}
